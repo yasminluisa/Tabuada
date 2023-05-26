e2bfdfe73f8909ad3409bb3115f2223867527d0b
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-05-26 19:36:04 -0300

    criando a tabuada do 5 e do 10

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("tabuada do 0");
+Console.WriteLine("inicio: ");
+int numero =0;
+int contador =1;
+
+while (contador <= 10)
+{
+    int resultado = numero * contador;
+    Console.WriteLine($"{numero} x {contador} = {resultado}");
+    Thread.Sleep(200);
+    contador = contador + 1;
+}
+
+Console.WriteLine("fim. ");
